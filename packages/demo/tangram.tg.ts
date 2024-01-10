02d0ded44fff67e542fe2a5fef2995b36f46ef0c
Ben Lovy <ben@deciduously.com>
2024-01-10 16:36:39 -0500

    fix(demo): update demo package

--- packages/demo/tangram.tg.ts
+++ packages/demo/tangram.tg.ts
@@ -1,23 +1,23 @@
 import nodejs from "tg:nodejs" with { path: "../nodejs" };
 import postgresql from "tg:postgresql" with { path: "../postgresql" };
+import ripgrep from "tg:ripgrep" with { path: "../ripgrep" };
 import * as std from "tg:std" with { path: "../std" };
 
 export let metadata = {
 	name: "demo",
 };
 
-export let container = tg.target(() => std.container(executable()));
+export let image = tg.target(async () => std.image(env(), { cmd: ["sh"] }));
 
 export let env = tg.target(() => std.env(...packages));
 
-export let executable = tg.target(async () =>
-	std.wrap(script, { env: env() }),
-);
+export let executable = tg.target(() => std.wrap(script, { env: env() }));
 
-let packages = [postgresql(), nodejs()];
+let packages = [nodejs(), postgresql(), ripgrep()];
 
 export let script = `
-	echo "Node.js version: $(node --version)" | tee $OUTPUT
+	echo "Node.js version: $(node --version)" | tee -a $OUTPUT
+	echo "ripgrep version: $(rg --version)" | tee -a $OUTPUT
 	echo "PostgreSQL version: $(psql --version)" | tee -a $OUTPUT
 `;
 
