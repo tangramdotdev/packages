0c9a43640763d7b51e8eb3e44ea07afe0b22c9ea
Ben Lovy <ben@deciduously.com>
2024-01-10 13:06:52 -0500

    fix(postgresql): provide openssl dependency

--- packages/postgresql/tangram.tg.ts
+++ packages/postgresql/tangram.tg.ts
@@ -1,4 +1,5 @@
 import ncurses from "tg:ncurses" with { path: "../ncurses" };
+import openssl from "tg:openssl" with { path: "../openssl" };
 import readline from "tg:readline" with { path: "../readline" };
 import * as std from "tg:std" with { path: "../std" };
 import zlib from "tg:zlib" with { path: "../zlib" };
@@ -44,15 +45,15 @@ export let postgresql = tg.target(async (arg?: Arg) => {
 		...rest
 	} = arg ?? {};
 
-	let env = [ncurses(arg), readline(arg), zlib(arg), env_];
+	let env = [ncurses(arg), openssl(arg), readline(arg), zlib(arg), env_];
 
 	let sourceDir = source_ ?? source();
 
- // NOTE - when building out of tree, the configure script breaks when it can't find /bin/pwd. A patch would be a better solution than copying the source.
+	// NOTE - when building out of tree, the configure script breaks when it can't find /bin/pwd. A patch would be a better solution than copying the source.
 	let prepare = tg`cp -R ${sourceDir}/* . && chmod -R u+w .`;
 	let configure = {
 		command: `./configure`,
-		args: ["--without-icu"]
+		args: ["--without-icu"],
 	};
 	let phases = { prepare, configure };
 
@@ -82,8 +83,11 @@ export let postgresql = tg.target(async (arg?: Arg) => {
 export default postgresql;
 
 export let test = tg.target(async () => {
-	return std.build(tg`
+	return std.build(
+		tg`
 		echo "Checking to see if we can run psql." | tee $OUTPUT
 		psql --version | tee -a $OUTPUT
-	`, { env: postgresql() });
+	`,
+		{ env: postgresql() },
+	);
 });
