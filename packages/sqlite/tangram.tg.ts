e8453046233201bff09a3fefb1c97f027e471d07
Ben Lovy <ben@deciduously.com>
2024-01-24 16:28:36 -0500

    fix(sqlite): add deps to build env
    * bump version
    * compute download package name
    * use standard assert

--- packages/sqlite/tangram.tg.ts
+++ packages/sqlite/tangram.tg.ts
@@ -4,16 +4,25 @@ import zlib from "tg:zlib" with { path: "../zlib" };
 
 export let metadata = {
 	name: "sqlite",
-	version: "3.44.2",
+	version: "3.45.0",
 };
 
 export let source = tg.target(async () => {
-	let { name } = metadata;
+	let { name, version } = metadata;
 	let checksum =
-		"sha256:1c6719a148bc41cf0f2bbbe3926d7ce3f5ca09d878f1246fcc20767b175bb407";
+		"sha256:72887d57a1d8f89f52be38ef84a6353ce8c3ed55ada7864eb944abd9a495e436";
 	let unpackFormat = ".tar.gz" as const;
-	let pkgName = `${name}-autoconf-3440200`;
-	let url = `https://www.sqlite.org/2023/${pkgName}${unpackFormat}`;
+
+	let produceVersion = (version: string) => {
+		let [major, minor, patch] = version.split(".");
+		tg.assert(major);
+		tg.assert(minor);
+		tg.assert(patch);
+		return `${major}${minor.padEnd(3, '0')}${patch.padEnd(3, '0')}`;
+	};
+
+	let pkgName = `${name}-autoconf-${produceVersion(version)}`;
+	let url = `https://www.sqlite.org/2024/${pkgName}${unpackFormat}`;
 	let download = tg.Directory.expect(
 		await std.download({
 			checksum,
@@ -34,12 +43,19 @@ type Arg = {
 };
 
 export let sqlite = tg.target((arg?: Arg) => {
-	let { autotools = [], build, host, source: source_, ...rest } = arg ?? {};
+	let { autotools = [], build, env: env_, host, source: source_, ...rest } = arg ?? {};
+
+	let dependencies = [
+		readline(arg),
+		zlib(arg),
+	];
+	let env = [...dependencies, env_];
 
 	return std.autotools.build(
 		{
 			...rest,
 			...std.Triple.rotate({ build, host }),
+			env,
 			source: source_ ?? source(),
 		},
 		autotools,
@@ -49,10 +65,11 @@ export let sqlite = tg.target((arg?: Arg) => {
 export default sqlite;
 
 export let test = tg.target(async () => {
-	let sqliteArtifact = sqlite();
-	await std.build(tg`
-		echo "Checking that we can run sqlite3." | tee $OUTPUT
-		${sqliteArtifact}/bin/sqlite3 -version | tee -a $OUTPUT
-	`);
-	return true;
+	let directory = sqlite();
+	await std.assert.pkg({
+		directory,
+		binaries: ["sqlite3"],
+		metadata
+	});
+	return directory;
 });
