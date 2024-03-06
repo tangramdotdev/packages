4f3c3b9d77d8dda2ef04dd68b53c51261e721f5f
Ben Lovy <ben@deciduously.com>
2024-03-06 14:26:44 -0500

    add perl package

---
+++ packages/perl/tangram.tg.ts
@@ -0,0 +1,166 @@
+import bison from "tg:bison" with { path: "../bison" };
+import libffi from "tg:libffi" with { path: "../libffi" };
+import m4 from "tg:m4" with { path: "../m4" };
+import * as std from "tg:std" with { path: "../std" };
+import zlib from "tg:zlib" with { path: "../zlib" };
+
+export let metadata = {
+	name: "perl",
+	version: "5.38.2",
+};
+
+export let source = tg.target(async () => {
+	let { name, version } = metadata;
+
+	// Download raw source.
+	let unpackFormat = ".tar.gz" as const;
+	let packageArchive = std.download.packageArchive({
+		name,
+		version,
+		unpackFormat,
+	});
+	let checksum =
+		"sha256:a0a31534451eb7b83c7d6594a497543a54d488bc90ca00f5e34762577f40655e";
+	let url = `https://www.cpan.org/src/5.0/${packageArchive}`;
+	let source = tg.Directory.expect(
+		await std.download({ url, checksum, unpackFormat }),
+	);
+	source = await std.directory.unwrap(source);
+
+	// Apply patches.
+	let patches = [];
+
+	let macosPatch = tg.File.expect(
+		await tg.include("./perl_macos_version.patch"),
+	);
+	patches.push(macosPatch);
+
+	let noFixDepsPatch = tg.File.expect(
+		await tg.include("./perl_no_fix_deps.patch"),
+	);
+	patches.push(noFixDepsPatch);
+
+	let cppPrecompPatch = tg.File.expect(
+		await tg.include("./perl_cpp_precomp.patch"),
+	);
+	patches.push(cppPrecompPatch);
+
+	return std.patch(source, ...(await Promise.all(patches)));
+});
+
+type Arg = {
+	autotools?: tg.MaybeNestedArray<std.autotools.Arg>;
+	build?: tg.Triple.Arg;
+	env?: std.env.Arg;
+	host?: tg.Triple.Arg;
+	sdk?: tg.MaybeNestedArray<std.sdk.Arg>;
+	source?: tg.Directory;
+};
+
+export let perl = tg.target(async (arg?: Arg) => {
+	let {
+		autotools = [],
+		build,
+		env: env_,
+		host,
+		source: source_,
+		...rest
+	} = arg ?? {};
+
+	let sourceDir = source_ ?? source();
+	let prepare = tg`cp -r ${sourceDir}/. . && chmod -R u+w .`;
+
+	let configure = {
+		args: [
+			"-des",
+			`-Dscriptdir=$OUTPUT/bin`,
+			"-Dinstallstyle=lib/perl5",
+			"-Dusethreads",
+			'-Doptimize="-O3 -pipe -fstack-protector -fwrapv -fno-strict-aliasing"',
+		],
+		command: "$SHELL Configure",
+	};
+
+	let phases = {
+		configure,
+		prepare,
+	};
+
+	let dependencies = [bison(arg), libffi(arg), m4(arg), zlib(arg)];
+	let env = [...dependencies, env_];
+
+	let perlArtifact = await std.autotools.build(
+		{
+			...rest,
+			...tg.Triple.rotate({ build, host }),
+			env,
+			phases,
+			prefixArg: "-Dprefix=",
+			source: sourceDir,
+		},
+		autotools,
+	);
+
+	let wrappedPerl = await std.wrap(
+		tg.symlink({ artifact: perlArtifact, path: "bin/perl" }),
+		{
+			identity: "wrapper",
+			env: {
+				PERL5LIB: tg.Mutation.templateAppend(
+					tg`${perlArtifact}/lib/perl5/${metadata.version}`,
+					":",
+				),
+			},
+			sdk: rest.sdk,
+		},
+	);
+
+	let scripts = [];
+	let binDir = tg.Directory.expect(await perlArtifact.get("bin"));
+	for await (let [name, artifact] of binDir) {
+		if (tg.File.is(artifact)) {
+			let metadata = await std.file.executableMetadata(artifact);
+			if (
+				metadata.format == "shebang" &&
+				metadata.interpreter.includes("perl")
+			) {
+				scripts.push(name);
+			}
+		}
+	}
+
+	for (let script of scripts) {
+		// Get the script artifact.
+		let scriptArtifact = tg.File.expect(
+			await perlArtifact.get(`bin/${script}`),
+		);
+
+		// Wrap it.
+		let wrappedScript = std.wrap(scriptArtifact, {
+			identity: "interpreter",
+			interpreter: wrappedPerl,
+			sdk: rest.sdk,
+		});
+
+		// Replace in the original artifact.
+		perlArtifact = await tg.directory(perlArtifact, {
+			[`bin/${script}`]: wrappedScript,
+		});
+	}
+
+	return tg.directory(perlArtifact, {
+		["bin/perl"]: wrappedPerl,
+	});
+});
+
+export default perl;
+
+export let test = tg.target(async () => {
+	let directory = perl();
+	await std.assert.pkg({
+		directory,
+		binaries: ["perl"],
+		metadata,
+	});
+	return directory;
+});
