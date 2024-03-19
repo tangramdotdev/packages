d7cd83c2360aba36b14b41760e1df10d3acb22f1
Ben Lovy <ben@deciduously.com>
2024-03-19 10:14:24 -0400

    chore(flex): update idioms/metadata

--- packages/flex/tangram.tg.ts
+++ packages/flex/tangram.tg.ts
@@ -1,24 +1,31 @@
+import help2man from "tg:help2man" with { path: "../help2man" };
+import m4 from "tg:m4" with { path: "../m4" };
 import * as std from "tg:std" with { path: "../std" };
+import texinfo from "tg:texinfo" with { path: "../texinfo" };
 
 export let metadata = {
+	homepage: "https://github.com/westes/flex",
+	license: "https://github.com/westes/flex/tree/master?tab=License-1-ov-file",
 	name: "flex",
+	repository: "https://github.com/westes/flex",
 	version: "2.6.4",
 };
 
-export let source = tg.target(async () => {
+export let source = tg.target(() => {
 	let { name, version } = metadata;
 	let checksum =
 		"sha256:e87aae032bf07c26f85ac0ed3250998c37621d95f8bd748b31f15b33c45ee995";
-	let unpackFormat = ".tar.gz" as const;
-	let url = `https://github.com/westes/${name}/releases/download/v${version}/${name}-${version}${unpackFormat}`;
-	let download = tg.Directory.expect(
-		await std.download({
-			checksum,
-			unpackFormat,
-			url,
-		}),
-	);
-	return std.directory.unwrap(download);
+	let owner = "westes";
+	let repo = name;
+	let tag = `v${version}`;
+	return std.download.fromGithub({
+		checksum,
+		owner,
+		repo,
+		tag,
+		release: true,
+		version,
+	});
 });
 
 type Arg = {
@@ -31,12 +38,23 @@ type Arg = {
 };
 
 export let flex = tg.target(async (arg?: Arg) => {
-	let { autotools = [], build, host, source: source_, ...rest } = arg ?? {};
+	let {
+		autotools = [],
+		build,
+		env: env_,
+		host,
+		source: source_,
+		...rest
+	} = arg ?? {};
+
+	let dependencies = [help2man(arg), m4(arg), texinfo(arg)];
+	let env = [...dependencies, env_];
 
 	return std.autotools.build(
 		{
 			...rest,
 			...tg.Triple.rotate({ build, host }),
+			env,
 			source: source_ ?? source(),
 		},
 		autotools,
@@ -45,9 +63,12 @@ export let flex = tg.target(async (arg?: Arg) => {
 
 export default flex;
 
-export let test = tg.target(() => {
-	return std.build(tg`
-		echo "Checking that we can run flex." | tee $OUTPUT
-		${flex()}/bin/flex --version | tee -a $OUTPUT
-	`);
+export let test = tg.target(async () => {
+	let directory = flex();
+	await std.assert.pkg({
+		directory,
+		binaries: ["flex"],
+		metadata,
+	});
+	return directory;
 });
