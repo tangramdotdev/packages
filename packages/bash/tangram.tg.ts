99047750120292274a6af3ac1655f5afc912a7ed
Ben Lovy <ben@deciduously.com>
2024-03-19 02:40:21 -0400

    feat(bash): implement bash.wrapScript

--- packages/bash/tangram.tg.ts
+++ packages/bash/tangram.tg.ts
@@ -1,8 +1,13 @@
+import gettext from "tg:gettext" with { path: "../gettext" };
+import ncurses from "tg:ncurses" with { path: "../ncurses" };
 import * as std from "tg:std" with { path: "../std" };
 
 export let metadata = {
+	homepage: "https://www.gnu.org/software/bash/",
+	license: "GPL-3.0-or-later",
 	name: "bash",
-	version: "5.2.15",
+	repository: "https://git.savannah.gnu.org/git/bash.git",
+	version: "5.2.21",
 };
 
 export let source = tg.target(async (arg?: Arg) => {
@@ -11,7 +16,7 @@ export let source = tg.target(async (arg?: Arg) => {
 	let env = std.env.object([std.sdk({ host: build }, arg?.sdk), arg?.env]);
 
 	let checksum =
-		"sha256:13720965b5f4fc3a0d4b61dd37e7565c741da9a5be24edc2ae00182fc1b3588c";
+		"sha256:c8e31bdc59b69aaffc5b36509905ba3e5cbb12747091d27b4b977f078560d5b8";
 	let source = std.download.fromGnu({ name, version, checksum });
 	// See https://lists.gnu.org/archive/html/bug-bash/2022-10/msg00000.html
 	// We don't have autoreconf available so we additionally manually resolve the configure script change. The m4 change isn't used, just here for completeness.
@@ -41,17 +46,28 @@ type Arg = {
 };
 
 export let bash = tg.target((arg?: Arg) => {
-	let { autotools = [], build, host, source: source_, ...rest } = arg ?? {};
+	let {
+		autotools = [],
+		build,
+		env: env_,
+		host,
+		source: source_,
+		...rest
+	} = arg ?? {};
 
 	let configure = {
-		args: ["--without-bash-malloc"],
+		args: ["--without-bash-malloc", "--with-curses"],
 	};
 	let phases = { configure };
+
+	let dependencies = [gettext(arg), ncurses(arg)];
+	let env = [...dependencies, env_];
 
 	return std.autotools.build(
 		{
 			...rest,
 			...tg.Triple.rotate({ build, host }),
+			env,
 			phases,
 			source: source_ ?? source(),
 		},
@@ -59,12 +75,28 @@ export let bash = tg.target((arg?: Arg) => {
 	);
 });
 
-export let test = tg.target(() => {
-	return std.build(tg`
-		mkdir -p $OUTPUT
-		echo "Checking that we can run bash scripts." > $OUTPUT/log.txt
-		${bash()}/bin/bash -c "echo 'Hello, world!' > $OUTPUT/hello.txt"
-	`);
-});
-
 export default bash;
+
+/** Wrap a shebang'd bash script to use this package's bach as the interpreter.. */
+export let wrapScript = async (script: tg.File) => {
+	let scriptMetadata = await std.file.executableMetadata(script);
+	if (
+		scriptMetadata?.format !== "shebang" ||
+		(scriptMetadata.interpreter !== "sh" &&
+			scriptMetadata.interpreter !== "bash")
+	) {
+		throw new Error("Expected a shebang sh or bash script");
+	}
+	let interpreter = tg.File.expect(await (await bash()).get("bin/bash"));
+	return std.wrap(script, { interpreter, identity: "executable" });
+};
+
+export let test = tg.target(async () => {
+	let directory = bash();
+	await std.assert.pkg({
+		directory,
+		binaries: ["bash"],
+		metadata,
+	});
+	return directory;
+});
