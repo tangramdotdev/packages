56eb1e80f7ef155de5883c2ba6eeb272d3daa113
Ben Lovy <ben@deciduously.com>
2024-03-19 09:43:12 -0400

    feat(libxcrypt): add package

---
+++ packages/libxcrypt/tangram.tg.ts
@@ -0,0 +1,79 @@
+import perl from "tg:perl" with { path: "../perl" };
+import * as std from "tg:std" with { path: "../std" };
+
+export let metadata = {
+	homepage: "https://github.com/besser82/libxcrypt",
+	name: "libxcrypt",
+	license: "LGPL-2.1",
+	repository: "https://github.com/besser82/libxcrypt",
+	version: "4.4.36",
+};
+
+export let source = tg.target(() => {
+	let { name, version } = metadata;
+	let owner = "besser82";
+	let repo = name;
+	let tag = `v${version}`;
+	let compressionFormat = ".xz" as const;
+	let checksum =
+		"sha256:e5e1f4caee0a01de2aee26e3138807d6d3ca2b8e67287966d1fefd65e1fd8943";
+	return std.download.fromGithub({
+		checksum,
+		compressionFormat,
+		owner,
+		release: true,
+		repo,
+		tag,
+		version,
+	});
+});
+
+type Arg = {
+	autotools?: tg.MaybeNestedArray<std.autotools.Arg>;
+	build?: tg.Triple.Arg;
+	env?: std.env.Arg;
+	host?: tg.Triple.Arg;
+	sdk?: tg.MaybeNestedArray<std.sdk.Arg>;
+	source?: tg.Directory;
+};
+
+export let build = tg.target(async (arg?: Arg) => {
+	let {
+		autotools = [],
+		build,
+		env: env_,
+		host,
+		source: source_,
+		...rest
+	} = arg ?? {};
+
+	let configure = {
+		args: ["--disable-dependency-tracking"],
+	};
+	let phases = { configure };
+
+	let dependencies = [perl(arg)];
+	let env = [...dependencies, env_];
+
+	return std.autotools.build(
+		{
+			...rest,
+			...tg.Triple.rotate({ build, host }),
+			env,
+			phases,
+			source: source_ ?? source(),
+		},
+		autotools,
+	);
+});
+
+export default build;
+
+export let test = tg.target(async () => {
+	let directory = build();
+	await std.assert.pkg({
+		directory,
+		libs: ["crypt"],
+	});
+	return directory;
+});
