8fc6293b209d14b3d26f1f1398da765bef47ba7f
Ben Lovy <ben@deciduously.com>
2024-03-19 02:42:39 -0400

    feat(bc): add package

---
+++ packages/bc/tangram.tg.ts
@@ -0,0 +1,91 @@
+import * as std from "tg:std" with { path: "../std" };
+
+export let metadata = {
+	homepage: "https://git.gavinhoward.com/gavin/bc",
+	name: "bc",
+	license: "BSD-2-Clause",
+	repository: "https://git.gavinhoward.com/gavin/bc",
+	version: "6.7.5",
+};
+
+export let source = tg.target(async () => {
+	let { name, version } = metadata;
+	let unpackFormat = ".tar.xz" as const;
+	let packageName = std.download.packageArchive({
+		name,
+		version,
+		unpackFormat,
+	});
+	let checksum =
+		"sha256:c3e02c948d51f3ca9cdb23e011050d2d3a48226c581e0749ed7cbac413ce5461";
+	let url = `https://git.gavinhoward.com/gavin/${name}/releases/download/${version}/${packageName}`;
+	let outer = tg.Directory.expect(
+		await std.download({
+			url,
+			checksum,
+			unpackFormat,
+		}),
+	);
+	return std.directory.unwrap(outer);
+});
+
+type Arg = {
+	autotools?: tg.MaybeNestedArray<std.autotools.Arg>;
+	build?: tg.Triple.Arg;
+	env?: std.env.Arg;
+	host?: tg.Triple.Arg;
+	sdk?: tg.MaybeNestedArray<std.sdk.Arg>;
+	source?: tg.Directory;
+};
+
+export let bc = tg.target(async (arg?: Arg) => {
+	let {
+		autotools = [],
+		build: build_,
+		env: env_,
+		host: host_,
+		source: source_,
+		...rest
+	} = arg ?? {};
+
+	let host = await tg.Triple.host(host_);
+	let build = build_ ? tg.triple(build_) : host;
+
+	let sourceDir = source_ ?? source();
+
+	// Define phases
+	let configure = {
+		args: ["--disable-nls", "--opt=3"],
+	};
+
+	// Define environment.
+	let ccCommand = build.os == "darwin" ? "cc -D_DARWIN_C_SOURCE" : "cc";
+	let env = [{ CC: tg.Mutation.setIfUnset(ccCommand) }, env_];
+
+	let output = std.autotools.build(
+		{
+			...rest,
+			...tg.Triple.rotate({ build, host }),
+			buildInTree: true,
+			env,
+			opt: "3",
+			phases: { configure },
+			source: sourceDir,
+		},
+		autotools,
+	);
+
+	return output;
+});
+
+export default bc;
+
+export let test = tg.target(async () => {
+	let directory = bc();
+	await std.assert.pkg({
+		directory,
+		binaries: ["bc"],
+		metadata,
+	});
+	return directory;
+});
