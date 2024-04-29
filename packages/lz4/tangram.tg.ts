2669b4aed32e198321bdd6f050c007fab419ba4d
Ben Lovy <ben@deciduously.com>
2024-04-29 13:46:56 -0400

    feat(lz4): create lz4 package

---
+++ packages/lz4/tangram.tg.ts
@@ -0,0 +1,67 @@
+import * as std from "tg:std" with { path: "../std" };
+
+export let metadata = {
+	homepage: "https://lz4.org/",
+	license:
+		"https://github.com/lz4/lz4/blob/5ff839680134437dbf4678f3d0c7b371d84f4964/LICENSE",
+	name: "lz4",
+	repository: "https://github.com/lz4/lz4",
+	version: "1.9.4",
+};
+
+export let source = tg.target(() => {
+	let { name, version } = metadata;
+	let checksum =
+		"sha256:0b0e3aa07c8c063ddf40b082bdf7e37a1562bda40a0ff5272957f3e987e0e54b";
+	let owner = name;
+	let repo = name;
+	let tag = `v${version}`;
+	return std.download.fromGithub({
+		checksum,
+		owner,
+		repo,
+		source: "release",
+		tag,
+		version,
+	});
+});
+
+type Arg = {
+	autotools?: tg.MaybeNestedArray<std.autotools.Arg>;
+	build?: string;
+	env?: std.env.Arg;
+	host?: string;
+	sdk?: tg.MaybeNestedArray<std.sdk.Arg>;
+	source?: tg.Directory;
+};
+
+export let lz4 = tg.target(async (arg?: Arg) => {
+	let { autotools = [], build, host, source: source_, ...rest } = arg ?? {};
+
+	let sourceDir = source_ ?? source();
+
+	let install = {
+		args: ["prefix=$OUTPUT"],
+	};
+	let phases = { configure: tg.Mutation.unset(), install };
+
+	return std.autotools.build({
+		...rest,
+		...std.triple.rotate({ build, host }),
+		buildInTree: true,
+		phases,
+		source: sourceDir,
+	});
+});
+
+export default lz4;
+
+export let test = tg.target(async () => {
+	let artifact = lz4();
+	await std.assert.pkg({
+		buildFunction: lz4,
+		binaries: ["lz4"],
+		libraries: ["lz4"],
+	});
+	return artifact;
+});
