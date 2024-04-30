5aaa1b6a335240badc956426cab3522eb29df717
Ben Lovy <ben@deciduously.com>
2024-04-30 02:28:45 -0400

    no zlib, testNative ok

--- packages/std/sdk/llvm.tg.ts
+++ packages/std/sdk/llvm.tg.ts
@@ -55,7 +55,6 @@ export let toolchain = tg.target(async (arg?: LLVMArg) => {
 	sysroot = tg.Directory.expect(await sysroot.get(host));
 	console.log("llvm sysroot", await sysroot.id());
 
-	let zlibArtifact = await dependencies.zlib.build({ host: build });
 	let deps: tg.Unresolved<std.env.Arg> = [
 		std.utils.env({ host: build }),
 		git({ host: build }),
@@ -63,7 +62,6 @@ export let toolchain = tg.target(async (arg?: LLVMArg) => {
 			host: build,
 			sdk: bootstrap.sdk.arg(build),
 		}),
-		zlibArtifact,
 	];
 
 	let env = [...deps, env_];
@@ -91,7 +89,6 @@ export let toolchain = tg.target(async (arg?: LLVMArg) => {
 			"-DLLVM_INSTALL_BINUTILS_SYMLINKS=ON",
 			"-DLLVM_INSTALL_TOOLCHAIN_ONLY=ON",
 			"-DLLVM_PARALLEL_LINK_JOBS=1",
-			tg`-DZLIB_ROOT=${zlibArtifact}`,
 		],
 	};
 
