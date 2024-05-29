54c7055403410401eb1ca8854b9b2226de62a39e
Ben Lovy <ben@deciduously.com>
2024-05-29 10:21:21 -0400

    fix(std): no target for directory.unwrap

--- packages/std/directory.tg.ts
+++ packages/std/directory.tg.ts
@@ -1,6 +1,6 @@
 import * as std from "./tangram.tg.ts";
 
 /** If the given directory contains a single child directory, return the inner child. */
-export let unwrap = tg.target(async (directory: tg.Directory) => {
+export let unwrap = async (directory: tg.Directory) => {
 	return await std.download.unwrapDirectory(directory);
-});
+};
