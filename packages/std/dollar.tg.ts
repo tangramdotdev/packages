16a928586a79028f14fafd65f395b91f97245c84
Ben Lovy <ben@deciduously.com>
2024-06-12 09:01:19 -0400

    .

--- packages/std/dollar.tg.ts
+++ packages/std/dollar.tg.ts
@@ -8,21 +8,21 @@ export function $(
 }
 
 class Dollar {
-	strings: TemplateStringsArray;
-	placeholders: std.Args<tg.Template.Arg>;
-	host?: string;
-	executable?: tg.Artifact | undefined;
-	args?: Array<tg.Value>;
-	env_?: { [key: string]: tg.Value };
-	lock?: tg.Lock | undefined;
-	checksum?: tg.Checksum | undefined;
+	#strings: TemplateStringsArray;
+	#placeholders: std.Args<tg.Template.Arg>;
+	#host?: string;
+	#executable?: tg.Artifact | undefined;
+	#args?: Array<tg.Value>;
+	#env?: { [key: string]: tg.Value };
+	#lock?: tg.Lock | undefined;
+	#checksum?: tg.Checksum | undefined;
 
 	constructor(
 		strings: TemplateStringsArray,
 		...placeholders: std.Args<tg.Template.Arg>
 	) {
-		this.strings = strings;
-		this.placeholders = placeholders;
+		this.#strings = strings;
+		this.#placeholders = placeholders;
 	}
 
 	env(): Dollar {
@@ -30,34 +30,38 @@ class Dollar {
 	}
 
 	async output(): Promise<tg.Value> {
+		return await (await this.target()).output();
+	}
+
+	async target(): Promise<tg.Target> {
 		let arg: tg.Target.ArgObject = {};
-		if (this.host !== undefined) {
-			arg.host = this.host;
+		if (this.#host !== undefined) {
+			arg.host = this.#host;
 		}
-		if (this.executable !== undefined) {
-			arg.executable = this.executable;
+		if (this.#executable !== undefined) {
+			arg.executable = this.#executable;
 		} else {
 			arg.executable = await tg.symlink({ path: tg.path("/bin/sh") });
 		}
-		arg.args = ["-c", await tg(this.strings, this.placeholders)];
-		if (this.args !== undefined) {
-			arg.args.push(...this.args);
+		arg.args = ["-c", await tg(this.#strings, this.#placeholders)];
+		if (this.#args !== undefined) {
+			arg.args.push(...this.#args);
 		}
-		if (this.env_ !== undefined) {
-			arg.env = this.env_;
+		if (this.#env !== undefined) {
+			arg.env = this.#env;
 		}
-		if (this.lock !== undefined) {
-			arg.lock = this.lock;
+		if (this.#lock !== undefined) {
+			arg.lock = this.#lock;
 		}
-		if (this.checksum !== undefined) {
-			arg.checksum = this.checksum;
+		if (this.#checksum !== undefined) {
+			arg.checksum = this.#checksum;
 		}
-		if (this.host !== undefined) {
-			arg.host = this.host;
+		if (this.#host !== undefined) {
+			arg.host = this.#host;
 		} else {
 			arg.host = await std.triple.host();
 		}
-		return await (await tg.target(arg)).output();
+		return await tg.target(arg);
 	}
 
 	then<TResult1 = tg.Value, TResult2 = never>(
@@ -75,6 +79,6 @@ class Dollar {
 }
 
 export let test = tg.target(async () => {
-	let output = await $`echo 'hi from dollar' > $OUTPUT`;
+	let output = await $`echo 'hi from dollar!!' > $OUTPUT`;
 	return output;
 });
