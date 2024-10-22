6b530c8869a010cf8ac065a881b2f3157447f4a8
Ben Lovy <ben@deciduously.com>
2024-10-22 16:07:11 -0400

    chore: remove test packages

--- packages/sanity/tangram.ts
+++
@@ -1,6 +0,0 @@
-export const metadata = {
-	name: "sanity",
-	version: "0.0.0",
-};
-
-export default tg.target(async () => 5);
--- packages/webdemo/tangram.ts
+++
@@ -1,4 +0,0 @@
-import jq from "jq";
-import sqlite from "sqlite";
-import * as std from "std";
-export default tg.target(() => std.env(jq(), sqlite()));
