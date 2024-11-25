aa246c980c1437182d98bdec98d0d3f4549273cd
Ben Lovy <ben@deciduously.com>
2024-11-25 10:29:36 -0500

    feat(mpdecimal): define package

---
+++ packages/mpdecimal/tangram.ts
@@ -0,0 +1,68 @@
+import * as std from "std" with { path: "../std" };
+
+export const metadata = {
+	homepage: "https://www.bytereef.org/mpdecimal/index.html",
+	license: "Simplified BSD",
+	name: "mpdecimal",
+	version: "4.0.0",
+};
+
+export const source = tg.target(async (): Promise<tg.Directory> => {
+	const { name, version } = metadata;
+	const checksum =
+		"sha256:942445c3245b22730fd41a67a7c5c231d11cb1b9936b9c0f76334fb7d0b4468c";
+	const base = `https://www.bytereef.org/software/mpdecimal/releases`;
+	const extension = ".tar.gz";
+	return std
+		.download({
+			checksum,
+			base,
+			name,
+			version,
+			extension,
+		})
+		.then(tg.Directory.expect)
+		.then(std.directory.unwrap);
+});
+
+export type Arg = {
+	autotools?: std.autotools.Arg;
+	build?: string;
+	env?: std.env.Arg;
+	host?: string;
+	sdk?: std.sdk.Arg;
+	source?: tg.Directory;
+};
+
+export const default_ = tg.target(async (...args: std.Args<Arg>) => {
+	const {
+		autotools = {},
+		build,
+		env,
+		host,
+		sdk,
+		source: source_,
+	} = await std.args.apply<Arg>(...args);
+
+	return std.autotools.build(
+		{
+			...(await std.triple.rotate({ build, host })),
+			env,
+			sdk,
+			source: source_ ?? source(),
+		},
+		autotools,
+	);
+});
+
+export default default_;
+
+export const test = tg.target(async () => {
+	await std.assert.pkg({
+		buildFn: default_,
+		headers: ["mpdecimal.h"],
+		libraries: ["mpdec"],
+		pkgConfigName: "libmpdec",
+	});
+	return true;
+});
