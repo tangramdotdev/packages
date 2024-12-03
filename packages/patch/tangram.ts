919aed9d269e14fadbd24e6ac1602e17d0af4fc6
Ben Lovy <ben@deciduously.com>
2024-12-03 10:50:10 -0500

    fix(patch): apply rlimit patch to post-std package

--- packages/patch/tangram.ts
+++ packages/patch/tangram.ts
@@ -1,5 +1,6 @@
 import * as std from "std" with { path: "../std" };
 import { $ } from "std" with { path: "../std" };
+import patches from "./patches" with { type: "directory" };
 
 export const metadata = {
 	homepage: "https://savannah.gnu.org/projects/patch/",
@@ -9,11 +10,12 @@ export const metadata = {
 	version: "2.7.6",
 };
 
-export const source = tg.target(() => {
+export const source = tg.target(async () => {
 	const { name, version } = metadata;
 	const checksum =
 		"sha256:8cf86e00ad3aaa6d26aca30640e86b0e3e1f395ed99f189b06d4c9f74bc58a4e";
-	return std.download.fromGnu({ name, version, checksum });
+	return std.download.fromGnu({ name, version, checksum })
+		.then((source) => std.patch(source, patches));
 });
 
 type Arg = {
