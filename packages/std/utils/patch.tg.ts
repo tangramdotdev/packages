cceb34a4c5115a5eb471349ebe0e3e72d72da8aa
Ben Lovy <ben@deciduously.com>
2024-12-03 10:45:37 -0500

    fix(std): apply rlimit fix to GNU patch
    closes #120

--- packages/std/utils/patch.tg.ts
+++ packages/std/utils/patch.tg.ts
@@ -1,22 +1,27 @@
 import * as bootstrap from "../bootstrap.tg.ts";
 import * as std from "../tangram.ts";
 import { buildUtil, prerequisites } from "../utils.tg.ts";
+import rlimitFix from "./patch-rlimit-fix.patch" with { type: "file" };
 
 export const metadata = {
 	name: "patch",
 	version: "2.7.6",
 };
 
-export const source = tg.target(() => {
+export const source = tg.target(async () => {
 	const { name, version } = metadata;
 	const checksum =
 		"sha256:ac610bda97abe0d9f6b7c963255a11dcb196c25e337c61f94e4778d632f1d8fd";
-	return std.download.fromGnu({
+	let source = await std.download.fromGnu({
 		name,
 		version,
 		compressionFormat: "xz",
 		checksum,
 	});
+	// Apply rlimit fix.
+	// See https://savannah.gnu.org/bugs/index.php?62958
+	source = await bootstrap.patch(source, rlimitFix);
+	return source;
 });
 
 export type Arg = {
