da0073e939af4b2f4707be880263ad765a58f2f1
Ben Lovy <ben@deciduously.com>
2025-01-13 18:05:18 -0500

    add options to dollar

--- packages/std/dollar.tg.ts
+++ packages/std/dollar.tg.ts
@@ -8,13 +8,17 @@ export function $(
 }
 
 class Dollar {
-	#strings: TemplateStringsArray;
-	#placeholders: std.args.UnresolvedArgs<tg.Template.Arg>;
-	#host?: string;
-	#executable?: tg.Artifact | undefined;
 	#args?: Array<tg.Value>;
-	#env?: std.args.UnresolvedArgs<std.env.Arg>;
 	#checksum?: tg.Checksum | undefined;
+	#disallowUnset: boolean;
+	#env?: std.args.UnresolvedArgs<std.env.Arg>;
+	#executable?: tg.Artifact | undefined;
+	#exitOnErr: boolean;
+	#includeUtils: boolean;
+	#host?: string;
+	#pipefail: boolean;
+	#placeholders: std.args.UnresolvedArgs<tg.Template.Arg>;
+	#strings: TemplateStringsArray;
 
 	constructor(
 		strings: TemplateStringsArray,
@@ -22,10 +26,19 @@ class Dollar {
 	) {
 		this.#strings = strings;
 		this.#placeholders = placeholders;
+		this.#disallowUnset = true;
+		this.#exitOnErr = true;
+		this.#includeUtils = true;
+		this.#pipefail = true;
 	}
 
 	checksum(checksum: tg.Checksum | undefined): Dollar {
 		this.#checksum = checksum;
+		return this;
+	}
+
+	disallowUnset(bool: boolean): Dollar {
+		this.#disallowUnset = bool;
 		return this;
 	}
 
@@ -38,23 +51,37 @@ class Dollar {
 		this.#executable = executable;
 		return this;
 	}
+	
+	exitOnErr(bool: boolean): Dollar {
+		this.#exitOnErr = bool;
+		return this;
+	}
 
 	host(host: string): Dollar {
 		this.#host = host;
 		return this;
 	}
 
+	includeUtils(bool: boolean): Dollar {
+		this.#includeUtils = bool;
+		return this;
+	}
+
 	async output(): Promise<tg.Value> {
 		return await (await this.target()).output();
+	}
+	
+	pipefail(bool: boolean): Dollar {
+		this.#pipefail = bool;
+		return this;
 	}
 
 	async target(): Promise<tg.Target> {
 		const arg: tg.Target.ArgObject = {};
-		if (this.#host !== undefined) {
-			arg.host = this.#host;
-		}
-		// If the user specified a custom executable, use that.
+
+		// Construct the executable.
 		if (this.#executable !== undefined) {
+			// If the user specified a custom executable, use that.
 			arg.executable = this.#executable;
 		} else {
 			// Otherwise, use the default bash executable from the standard utils.
@@ -63,20 +90,40 @@ class Dollar {
 				.then((dir) => dir.get("bin/bash"))
 				.then(tg.File.expect);
 		}
-		arg.args = [
-			"-c",
-			await tg(this.#strings, ...std.flatten(this.#placeholders)),
-		];
+
+		// Construct the args.
+		arg.args = [];
+		if (this.#disallowUnset) {
+			arg.args.push("-u");
+		}
+		if (this.#exitOnErr) {
+			arg.args.push("-e");
+		}
+		if (this.#pipefail) {
+			arg.args.push("-o");
+			arg.args.push("pipefail");
+		}
+		arg.args.push("-c");
+		arg.args.push(await tg(this.#strings, ...std.flatten(this.#placeholders)));
 		if (this.#args !== undefined) {
 			arg.args.push(...this.#args);
 		}
-		// Ensure the standard utils are provided in the env.
-		const env_ = std.utils.env({ sdk: false, env: std.sdk(), host: arg.host });
-		if (this.#env !== undefined) {
-			arg.env = await std.env.arg(env_, this.#env);
+
+		// Construct the env.
+		if (this.#includeUtils) {
+			const utilsEnv = std.utils.env({ sdk: false, env: std.sdk(), host: arg.host });
+			if (this.#env !== undefined) {
+				arg.env = await std.env.arg(utilsEnv, this.#env);
+			} else {
+				arg.env = await utilsEnv;
+			}
 		} else {
-			arg.env = await env_;
+			if (this.#env !== undefined) {
+				arg.env = std.env.arg(this.#env);
+			}
 		}
+
+		// Set remaining fields.
 		if (this.#checksum !== undefined) {
 			arg.checksum = this.#checksum;
 		}
