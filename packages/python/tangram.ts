3ca86535de86e3a58d981aa5181a8643e2602844
Ben Lovy <ben@deciduously.com>
2025-04-28 15:06:41 -0400

    fix(python): add DYLD_LIBRARY_PATH explicitly to configure phase on macOS

--- packages/python/tangram.ts
+++ packages/python/tangram.ts
@@ -148,6 +148,7 @@ export const self = tg.command(async (...args: std.Args<Arg>) => {
 	const makeArgs = [];
 	if (os === "darwin") {
 		envs.push({ MACOSX_DEPLOYMENT_TARGET: "15.2" });
+		configureArgs.push("DYLD_FALLBACK_LIBRARY_PATH=$DYLD_FALLBACK_LIBRARY_PATH");
 		makeArgs.push(
 			"RUNSHARED=DYLD_FALLBACK_LIBRARY_PATH=$DYLD_FALLBACK_LIBRARY_PATH",
 		);
