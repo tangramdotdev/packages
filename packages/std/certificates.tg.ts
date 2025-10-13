f2c59826f94724d3515c00dcce0a7d8fa9579f7b
Ben Lovy <ben@deciduously.com>
2025-10-13 12:33:54 -0400

    chore: bump client, deps, certificates

--- packages/std/certificates.tg.ts
+++ packages/std/certificates.tg.ts
@@ -1,6 +1,6 @@
 const metadata = {
 	name: "ca-certificates",
-	version: "2025-07-15",
+	version: "2025-09-09",
 };
 
 export type Arg = {
@@ -10,7 +10,7 @@ export type Arg = {
 export const caCertificates = (arg?: Arg) => {
 	const { version } = metadata;
 	const checksum =
-		"sha256:7430e90ee0cdca2d0f02b1ece46fbf255d5d0408111f009638e3b892d6ca089c";
+		"sha256:f290e6acaf904a4121424ca3ebdd70652780707e28e8af999221786b86bb1975";
 	const url = "https://curl.se/ca";
 	const source =
 		arg?.source ?? tg.download(`${url}/cacert-${version}.pem`, checksum);
