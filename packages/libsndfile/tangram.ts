f31d5af67c9b9f06d2871415fe32a3b8ebe5eac0
Mike Hilgendorf <mike@hilgendorf.audio>
2025-11-14 16:52:15 -0600

    feat: add xiph.org packages (opus, vorbis, ogg, flac) and libsndfile

---
+++ packages/libsndfile/tangram.ts
@@ -0,0 +1,55 @@
+import * as cmake from "cmake" with { local: "../cmake" };
+import * as flac from "flac" with { local: "../flac" };
+import * as ogg from "ogg" with { local: "../ogg" };
+import * as opus from "opus" with { local: "../opus" };
+import * as vorbis from "vorbis" with { local: "../vorbis" };
+import * as std from "std" with { local: "../std" };
+
+export const metadata = {
+	license: "LGPL-2.1",
+	name: "libsndfile",
+	repository: "https://github.com/libsndfile/libsndfile",
+	version: "1.2.2",
+	tag: "libsndfile/1.2.2",
+	provides: {
+		libraries: ["sndfile"],
+	},
+};
+
+export const source = async (): Promise<tg.Directory> => {
+	const { name, version } = metadata;
+	const checksum =
+		"sha256:3799ca9924d3125038880367bf1468e53a1b7e3686a934f098b7e1d286cdb80e";
+	const owner = "libsndfile";
+	const repo = name;
+	const tag = version;
+
+	return std.download.fromGithub({
+		checksum,
+		compression: "xz",
+		owner,
+		repo,
+		source: "release",
+		tag,
+		version,
+	});
+};
+
+export type Arg = cmake.Arg;
+
+export const build = async (...args:std.Args<Arg>) => {
+	return cmake.build(
+		{ source: source() },
+		{ env: std.env.arg(flac.env()) },
+		{ env: std.env.arg(ogg.env()) },
+		{ env: std.env.arg(opus.env()) },
+		{ env: std.env.arg(vorbis.env()) },
+		...args,
+	);
+};
+
+export const env = () => std.env.arg({
+	PKG_CONFIG_PATH: tg.Mutation.suffix(tg`${build()}/lib64/pkgconfig`, ":"),
+});
+
+export default build;
---
+++ packages/ogg/tangram.ts
@@ -0,0 +1,37 @@
+import * as std from "std" with { local: "../std" };
+import { $ } from "std" with { local: "../std" };
+import * as cmake from "cmake" with { local: "../cmake" };
+
+export const metadata = {
+	homepage: "https://xiph.org/ogg",
+	name: "ogg",
+	version: "1.3.6",
+	provides: {},
+};
+
+export const source = () => {
+	std.download;
+	const { name, version } = metadata;
+	const checksum =
+		"sha256:5c8253428e181840cd20d41f3ca16557a9cc04bad4a3d04cce84808677fa1061";
+	return std
+		.download({
+			url: `https://downloads.xiph.org/releases/ogg/libogg-${version}.tar.xz`,
+			checksum,
+			mode: "extract",
+		})
+		.then(tg.Directory.expect)
+		.then((d) => d.get(`lib${name}-${version}`))
+		.then(tg.Directory.expect);
+};
+
+export type Arg = cmake.Arg;
+
+export const build = (...args: std.Args<Arg>) =>
+	cmake.build({ source: source() }, ...args).then(tg.Directory.expect);
+
+export const env = () => std.env.arg({
+	PKG_CONFIG_PATH: tg.Mutation.suffix(tg`${build()}/lib64/pkgconfig`, ":"),
+});
+
+export default build;
