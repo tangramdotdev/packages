5b84531c9b110e3243e9c7dc34fdbf82ac59d193
Ben Lovy <ben@deciduously.com>
2025-11-26 12:22:05 -0500

    fix(strace): missing configure
    - bump to 6.17

--- packages/strace/tangram.ts
+++ packages/strace/tangram.ts
@@ -6,8 +6,8 @@ export const metadata = {
 	name: "strace",
 	license: "https://github.com/strace/strace/blob/master/COPYING",
 	repository: "https://github.com/strace/strace",
-	version: "6.10",
-	tag: "strace/6.10",
+	version: "6.17",
+	tag: "strace/6.17",
 	provides: {
 		binaries: ["strace"],
 	},
@@ -19,7 +19,7 @@ export const source = async () => {
 	const repo = name;
 	const tag = `v${version}`;
 	const checksum =
-		"sha256:765ec71aa1de2fe37363c1e40c7b7669fc1d40c44bb5d38ba8e8cd82c4edcf07";
+		"sha256:0a7c7bedc7efc076f3242a0310af2ae63c292a36dd4236f079e88a93e98cb9c0";
 	return std.download.fromGithub({
 		checksum,
 		compression: "xz",
@@ -52,10 +52,16 @@ export const build = async (...args: std.Args<Arg>) => {
 
 	std.assert.supportedHost(host, metadata);
 
+	const configure = {
+		args: ["--enable-mpers=check"],
+	};
+	const phases = { configure };
+
 	return std.autotools.build(
 		{
 			...(await std.triple.rotate({ build, host })),
 			env,
+			phases,
 			sdk,
 			source: source_ ?? source(),
 		},
