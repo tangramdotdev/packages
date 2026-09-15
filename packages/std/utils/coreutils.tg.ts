b61a233e2dcb8850489a69542c92a36c92aa9e28
Ben Lovy <ben@deciduously.com>
2026-09-15 10:36:12 -0400

    chore(coreutils,std): update coreutils to 9.12

--- packages/std/utils/coreutils.tg.ts
+++ packages/std/utils/coreutils.tg.ts
@@ -8,14 +8,14 @@ import alwaysPreserveXattrsPatch from "./coreutils-always-preserve-xattrs.patch"
 
 export const metadata = {
 	name: "coreutils",
-	version: "9.11",
-	tag: "coreutils/9.11",
+	version: "9.12",
+	tag: "coreutils/9.12",
 };
 
 export async function source(os: string) {
 	const { name, version } = metadata;
 	const checksum =
-		"sha256:394024eda0a5955217ceda9cd1201e65dc8fa3aa29c2951135a49521d57c3cc3";
+		"sha256:a480198559733e9b3da999e90543ac6f888a2caa544d8d664c5a1f17e528e210";
 	let source = await std.download.fromGnu({
 		name,
 		version,
